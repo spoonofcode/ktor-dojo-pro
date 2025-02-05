73ebc79c2a3233bbed5bfa1da3b0bfc68c7451ad
Luczak Bartosz <spoonofcode@gmail.com>
2025-02-05 21:38:33 +0100

    #3 Remove not needed println

--- src/main/kotlin/com/spoonofcode/routes/RefreshRoute.kt
+++ src/main/kotlin/com/spoonofcode/routes/RefreshRoute.kt
@@ -13,21 +13,17 @@ fun Route.refresh() {
     route("/refresh") {
         post("/") {
             val refreshToken = call.request.headers["Authorization"]?.removePrefix("Bearer ")
-            println("BARTEK refreshToken = $refreshToken")
             if (refreshToken.isNullOrBlank()) {
                 call.respondText("No refresh token provided", status = HttpStatusCode.BadRequest)
                 return@post
             }
 
             try {
-                println("BARTEK Step 1")
                 val verifier = JwtConfig.getVerifier()
                 val decodedJWT = verifier.verify(refreshToken)
-                println("BARTEK Step 2")
 
                 // Check if it's actually a refresh token
                 val isRefresh = decodedJWT.getClaim("refresh").asBoolean()
-                println("BARTEK isRefresh = $isRefresh")
                 if (!isRefresh) {
                     call.respondText("Not a refresh token", status = HttpStatusCode.BadRequest)
                     return@post
@@ -35,15 +31,9 @@ fun Route.refresh() {
 
                 val userId = decodedJWT.getClaim("userId").asString()
 
-                println("BARTEK userId = $userId")
-
                 // Generate new tokens
                 val newJwtAccessToken = JwtConfig.createAccessToken(userId)
                 val newJwtRefreshToken = JwtConfig.createRefreshToken(userId)
-
-                println("BARTEK newAccessToken = $newJwtAccessToken")
-                println("BARTEK newRefreshToken = $newJwtRefreshToken")
-
 
                 call.respond(
                     mapOf(
