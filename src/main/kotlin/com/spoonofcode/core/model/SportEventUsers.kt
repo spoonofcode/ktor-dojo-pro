db865b8539d84860a5cb48e57ca35a92eb959457
Luczak Bartosz <spoonofcode@gmail.com>
2025-02-21 20:34:18 +0100

    #13 Add missing onDelete and onUpdate options

--- src/main/kotlin/com/spoonofcode/core/model/SportEventUsers.kt
+++ src/main/kotlin/com/spoonofcode/core/model/SportEventUsers.kt
@@ -1,9 +1,15 @@
 package com.spoonofcode.core.model
 
+import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.Table
 
 object SportEventUsers : Table() {
-    val sportEventId = reference("sport_event_id", SportEvents.id)
+    val sportEventId = reference(
+        name = "sport_event_id",
+        refColumn = SportEvents.id,
+        onDelete = ReferenceOption.CASCADE,
+        onUpdate = ReferenceOption.CASCADE
+    )
     val userId = reference("user_id", Users.id)
     override val primaryKey = PrimaryKey(sportEventId, userId)
 }
