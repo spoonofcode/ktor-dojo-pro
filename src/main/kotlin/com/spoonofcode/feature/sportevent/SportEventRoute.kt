0dd407d78ca4ebd9669d133ecb21fce399748948
Luczak Bartosz <spoonofcode@gmail.com>
2025-02-25 14:15:34 +0100

    #13 Fix SportEventRoute

--- src/main/kotlin/com/spoonofcode/feature/sportevent/SportEventRoute.kt
+++ src/main/kotlin/com/spoonofcode/feature/sportevent/SportEventRoute.kt
@@ -1,21 +1,20 @@
 package com.spoonofcode.feature.sportevent
 
+import com.spoonofcode.core.base.ext.safeRespond
 import com.spoonofcode.core.base.routes.crudRoute
 import com.spoonofcode.core.data.repository.SportEventRepository
-import com.spoonofcode.core.domain.SportEventUseCase
+import com.spoonofcode.core.data.repository.SportEventUsersRepository
 import com.spoonofcode.core.model.AddUserToSportEventRequest
-import com.spoonofcode.feature.sporteventusers.SportEventUsersResult
 import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.plugins.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.koin.ktor.ext.get
 
 fun Route.sportEvents(
-    sportEventUseCase: SportEventUseCase = get(),
-    sportEventRepository: SportEventRepository = get()
+    sportEventRepository: SportEventRepository = get(),
+    sportEventUsersRepository: SportEventUsersRepository = get(),
 ) {
     val basePath = "/sportEvents"
     crudRoute(
@@ -26,18 +25,12 @@ fun Route.sportEvents(
         get("") {
             val creatorUserId = call.request.queryParameters["creatorUserId"]?.toIntOrNull()
             if (creatorUserId != null) {
-                when (val result = sportEventUseCase.getSportEventsCreatedByUser(creatorUserId = creatorUserId)) {
-                    is SportEventResult.Success -> {
-                        call.respond(HttpStatusCode.OK, result.sportEvents)
-                    }
-
-                    is SportEventResult.UnknownError -> {
-                        call.respond(HttpStatusCode.InternalServerError, result.message)
-                    }
+                call.safeRespond {
+                    val sportEventsByCreatorUserId = sportEventRepository.readByCreatorUserId(creatorUserId = creatorUserId)
+                    call.respond(HttpStatusCode.OK, sportEventsByCreatorUserId)
                 }
             } else {
-                throw BadRequestException("Missing 'creatorUserId' parameter.")
-
+                call.respond(HttpStatusCode.BadRequest, "Missing or invalid 'creatorUserId' parameter.")
             }
         }
 
@@ -46,16 +39,10 @@ fun Route.sportEvents(
             if (sportEventId != null) {
                 val addUserToSportEventRequest = call.receive<AddUserToSportEventRequest>()
                 val userId = addUserToSportEventRequest.userId
-
-                when (sportEventUseCase.addUserToSportEvent(userId = userId, sportEventId = sportEventId)) {
-                    SportEventUsersResult.Success -> {
-                        call.respond(HttpStatusCode.Created, "User with id = $userId added to event with id = $sportEventId.")
-                    }
-
-                    is SportEventUsersResult.UnknownError -> TODO()
-                }
+                sportEventUsersRepository.addUserToSportEvent(userId, sportEventId)
+                call.respond(HttpStatusCode.Created, "User with id = $userId added to event with id = $sportEventId.")
             } else {
-                throw BadRequestException("Missing 'sportEventId' parameter.")
+                call.respond(HttpStatusCode.BadRequest, "Missing or invalid 'sportEventId' parameter.")
             }
         }
     }
