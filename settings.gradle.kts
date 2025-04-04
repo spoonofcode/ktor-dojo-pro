216ac89f24f547bed32bee1195ce8e267454b99d
spoonofcode <102907783+spoonofcode@users.noreply.github.com>
2025-04-04 14:41:06 +0200

    #19 update libraries and plugins (#20)
    #19 Move versions to toml file and update libraries and plugins

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
 rootProject.name = "com.spoonofcode.ktor-dojo-pro"
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libraries") {
+            from(files("gradle/libs.versions.toml"))
+        }
+        create("plugins") {
+            from(files("gradle/libs.versions.toml"))
+        }
+    }
+}
