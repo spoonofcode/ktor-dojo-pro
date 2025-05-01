ef7f70f1455904d11b30b0a2bc89aa44adb407bc
Luczak Bartosz <spoonofcode@gmail.com>
2025-05-01 14:13:32 +0200

    #29 Migrate to new version for ktor and others libs

--- src/main/kotlin/com/spoonofcode/plugins/Monitoring.kt
+++ src/main/kotlin/com/spoonofcode/plugins/Monitoring.kt
@@ -1,9 +1,9 @@
 package com.spoonofcode.plugins
 
 import io.ktor.server.application.*
-import io.ktor.server.plugins.callloging.*
+import io.ktor.server.plugins.calllogging.*
 import io.ktor.server.request.*
-import org.slf4j.event.*
+import org.slf4j.event.Level
 
 fun Application.configureMonitoring() {
     install(CallLogging) {
