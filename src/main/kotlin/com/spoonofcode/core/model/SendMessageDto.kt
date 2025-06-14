78a1ffa8a5a6599ffe692d820472985d3138cba3
Luczak Bartosz <spoonofcode@gmail.com>
2025-06-14 14:34:52 +0200

    #31 Add push notification

--- src/main/kotlin/com/spoonofcode/core/model/SendMessageDto.kt
+++ src/main/kotlin/com/spoonofcode/core/model/SendMessageDto.kt
@@ -6,7 +6,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class SendMessageDto(
-    val to: String?,
+    val to: String? = null,
     val notification: NotificationBody,
 )
 
